53688628d963e726c9f463607fb49b96d055a567
milanakdj <milanakdj@gmail.com>
2021-08-27 08:50:34 +0545

    bad ai added and hitbox and hurtbox added as well

--- Enemies/Bat.gd
+++ Enemies/Bat.gd
@@ -2,18 +2,53 @@ extends KinematicBody2D
 
 const EnemyDeathEffect = preload("res://Effects/EnemyDeathEffect.tscn")
 
+export var ACCELERATION = 300
+export var MAX_SPEED = 300
+export var FRICTION = 200
+
+enum {
+	IDLE,
+	WANDER,
+	CHASE
+}
 var knockback = Vector2.ZERO
+var velocity = Vector2.ZERO
+
+var state = IDLE
 
 onready var stats = $Stats
+onready var playerDetectoinZone = $PlayerDetectionZone
+onready var sprite = $AnimatedSprite
 
 func _ready():
 	print(stats.max_health)
 	print(stats.health)
 	
 func _physics_process(delta):
-	knockback = knockback.move_toward(Vector2.ZERO,200 * delta)
+	knockback = knockback.move_toward(Vector2.ZERO, FRICTION * delta)
 	knockback = move_and_slide(knockback)
-
+	
+	match state:
+		IDLE:
+			velocity = velocity.move_toward(Vector2.ZERO,FRICTION * delta)
+			seek_player()
+		WANDER:
+			pass
+		CHASE:
+			var player = playerDetectoinZone.player
+			if player!=null:
+				var direction = (player.global_position - global_position).normalized()
+				velocity = velocity.move_toward(direction * MAX_SPEED, ACCELERATION * delta)
+			else: 
+				state = IDLE 
+			sprite.flip_h = velocity.x < 0
+			
+	velocity = move_and_slide(velocity)
+		
+func seek_player():
+	if playerDetectoinZone.can_see_player():
+		state = CHASE
+	
 func _on_HurtBox_area_entered(area):
 	stats.health -= area.damage #calling down and signaling up
 	knockback = area.knockback_vector * 150
